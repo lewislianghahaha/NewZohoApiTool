5869223ba0460d515b989872001ba918be110105
Lewis <heshanpin2016@163.com>
2023-07-11 15:23:02 +0800

    20230711:修改相关描述

--- ZohoApiTool/Main.cs
+++ ZohoApiTool/Main.cs
@@ -114,7 +114,7 @@ namespace ZohoApiTool
             _myTimer.AutoReset = true;
             _myTimer.Enabled = true;
 
-            //todo:主分三部份==>1.Elapsed 创建子线线程,用于根据不同条件进行相关操作
+            //todo:主要分三部份==>1.Elapsed 创建子线线程,用于根据不同条件进行相关操作
             //todo:2.ShowStartToContol() 及 ShowRdToControl() 为委托方法,主要用于通知委托更新指定UI线程内控件的值
             //todo:3.GenerateRecord() 核心运算方法;主体执行在此,并最后将返回值传输至ShowRdToControl() 委托方法进行更改控件显示
             _myTimer.Elapsed += (a1, a2) =>
